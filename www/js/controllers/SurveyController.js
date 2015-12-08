3767c99e6cc965ccdf53ae30b223a42411b9620a
Vo Thanh Quang <thanhquang2608@gmail.com>
2015-12-08 15:41:08 +0700

    Try set enableHighAccuracy: false

--- www/js/controllers/SurveyController.js
+++ www/js/controllers/SurveyController.js
@@ -1589,7 +1589,7 @@
         _handleRefresh: function () {
             var options = {
                 frequency: 5000,
-                enableHighAccuracy: true,
+                enableHighAccuracy: false,
                 timeout: 30000
             };
 
@@ -1628,7 +1628,7 @@
             // Update the watch every second.
             var options = {
                 frequency: 5000,
-                enableHighAccuracy: true,
+                enableHighAccuracy: false,
                 timeout: 30000
             };
 
